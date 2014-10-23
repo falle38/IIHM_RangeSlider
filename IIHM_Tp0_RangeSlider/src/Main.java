a9825407dbd0791a489a0db4b3f5c400412660be
falle38 <falle.malick@gmail.com>
2014-10-23 15:53:46 +0200

    Correction affichage

--- IIHM_Tp0_RangeSlider/src/Main.java
+++ IIHM_Tp0_RangeSlider/src/Main.java
@@ -7,21 +7,29 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		RangeSlider rangeSlider = new RangeSlider(20,60,0,300);
-		JFrame frame = new JFrame ("Range Slider");
-		frame.setAlwaysOnTop(true);
-		frame.setVisible(true);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(500, 200);
-		frame.setLocationRelativeTo(null);
-		frame.add(rangeSlider);
-		System.out.println(rangeSlider.getBoundedSlider().getMin());
-		System.out.println(rangeSlider.getBoundedSlider().getMax());
-		System.out.println(rangeSlider.getBoundedSlider().getLowerRange());
-		System.out.println(rangeSlider.getBoundedSlider().getUpperRange());
-		
-		
-	}
-
+        // TODO Auto-generated method stub
+        
+        int valeurmin = 0;
+        int valeurmax = 300;
+        int largeurpouce = 10; //d'après le paintcomponent dans RangeSlider.java
+   
+        RangeSlider rangeSlider = new RangeSlider(20,60,valeurmin,valeurmax);
+        JFrame frame = new JFrame ("Range Slider");
+        frame.setAlwaysOnTop(true);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+     
+        frame.setBounds(100,100,valeurmax - valeurmin + largeurpouce,200);
+     
+        
+        frame.setLocationRelativeTo(null);
+        frame.add(rangeSlider);
+        System.out.println(rangeSlider.getBoundedSlider().getMin());
+        System.out.println(rangeSlider.getBoundedSlider().getMax());
+        System.out.println(rangeSlider.getBoundedSlider().getLowerRange());
+        System.out.println(rangeSlider.getBoundedSlider().getUpperRange());
+        
+        
+    }
 }
